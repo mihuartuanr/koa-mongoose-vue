75cec3ebf66c421d20591142d718466b6c337e53
zhangchao <zhangchaouidesign@163.com>
2020-02-18 21:41:54 +0800

    前端项目初始化

---
+++ client/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+Login
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
